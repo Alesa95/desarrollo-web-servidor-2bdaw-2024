dbffc6cacda022f15096d285ec83a6015e4401b5
Alesa95 <alejandra_santiago_95@hotmail.com>
2024-10-21 12:49:52 +0200

    primera funcion

---
+++ 05_funciones/temperaturas.php
@@ -0,0 +1,29 @@
+<?php
+    //  vamos a crear una funcion que reciba la temperatura, la unidad
+    //  inicial y la final, y devuelva la temperatura final
+
+    function convertirTemperatura($temperaturaInicial, $unidadInicial, $unidadFinal) {
+        $temperaturaFinal = $temperaturaInicial;
+
+        if($unidadInicial == "C") {
+            if($unidadFinal == "K") {
+                $temperaturaFinal = $temperaturaInicial + 273.15;
+            } elseif($unidadFinal == "F") {
+                $temperaturaFinal = ($temperaturaInicial * (9/5)) + 32;
+            }
+        } elseif($unidadInicial == "K") {
+            if($unidadFinal == "C") {
+    
+            } elseif($unidadFinal == "F") {
+    
+            }
+        } elseif($unidadInicial == "F") {
+            if($unidadFinal == "C") {
+                
+            } elseif($unidadFinal == "K") {
+    
+            }
+        }
+        echo "<p>$temperaturaFinal</p>";
+    }
+?>
