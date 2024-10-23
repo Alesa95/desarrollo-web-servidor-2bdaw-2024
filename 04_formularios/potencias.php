7c1284c26edbf0da3dcdc0e4f417a8929f50e091
Alesa95 <alejandra_santiago_95@hotmail.com>
2024-10-23 10:31:26 +0200

    cosas

--- 04_formularios/potencias.php
+++ 04_formularios/potencias.php
@@ -7,6 +7,8 @@
     <?php
         error_reporting( E_ALL );
         ini_set("display_errors", 1 );    
+
+        require('../05_funciones/potencias.php');
     ?>
 </head>
 <body>
@@ -19,38 +21,67 @@
     </form>
 
     <?php
-    
     if($_SERVER["REQUEST_METHOD"] == "POST") {
-        echo "<p>Entramos en la respuesta</p>";
-        $base = $_POST["base"];
-        $exponente = $_POST["exponente"];
-        $resultado = 1;
+        $tmp_base = $_POST["base"];
+        $tmp_exponente = $_POST["exponente"];
 
-        for($i = 0; $i < $exponente; $i++) {
-            $resultado = $resultado * $base;
-            /**
-             * 2 elevado a 3
-             * 
-             * resultado = 1x2 = 2
-             * resultado = 2x2 = 4
-             * resultado = 4x2 = 8
-             */
+        /*
+        if($tmp_base != '') {
+            if(filter_var($tmp_base, FILTER_VALIDATE_INT) !== FALSE) {
+                $base = $tmp_base;
+            } else {
+                echo "<p>La base debe ser un número</p>";
+            }
+        } else {
+            echo "<p>La base es obligatoria</p>";
         }
-        echo "<h1>El resultado es $resultado</h1>";
+        */
+
+        if($tmp_base == '') {
+            echo "<p>La base es obligatoria</p>"; 
+        } else {
+            if(filter_var($tmp_base, FILTER_VALIDATE_INT) === FALSE) {
+                echo "<p>La base debe ser un número</p>";
+            } else {
+                $base = $tmp_base;
+            }
+        }
+
+        /*
+        if($tmp_exponente != '') {
+            if(filter_var($tmp_exponente, FILTER_VALIDATE_INT) !== FALSE) {
+                if($tmp_exponente >= 0) {
+                    $exponente = $tmp_exponente;
+                } else {
+                    echo "<p>El exponente debe ser mayor o igual que cero</p>";
+                }
+            } else {
+                echo "<p>El exponente debe ser un número</p>";
+            }
+        } else {
+            echo "<p>El exponente es obligatorio</p>";
+        }
+        */
+
+        if($tmp_exponente == '') {
+            echo "<p>El exponente es obligatorio</p>";
+        } else {
+            if(filter_var($tmp_exponente, FILTER_VALIDATE_INT) === FALSE) {
+                echo "<p>El exponente debe ser un número</p>";
+            } else {
+                if($tmp_exponente < 0) {
+                    echo "<p>El exponente debe ser mayor o igual que cero</p>";
+                } else {
+                    $exponente = $tmp_exponente;
+                }
+            }
+        }
+
+        if(isset($base) && isset($exponente)) {
+            $resultado = potencia($base, $exponente);
+            echo "<h1>El resultado es $resultado</h1>";
+        }
     }
-    /**
-     * CREAR UN FORMULARIO QUE RECIBA DOS PARÁMETROS: BASE Y EXPONENTE
-     * 
-     * CUANDO SE ENVÍE EL FORMULARIO, SE CALCULARÁ EL RESULTADO DE ELEVAR
-     * LA BASE AL EXPONENTE
-     * 
-     * EJEMPLOS:
-     * 
-     * 2 ELEVADO A 3 = 8 => 2x2x2 = 8
-     * 3 ELEVADO A 2 = 9 => 3x3 = 9
-     * 2 ELEVADO A 1 = 2
-     * 3 ELEVADO A 0 = 1
-     */
     ?>
 </body>
 </html>
