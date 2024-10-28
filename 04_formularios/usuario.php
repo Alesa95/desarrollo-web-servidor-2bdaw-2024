2ae4bd85a4e5f193df53f90f184d94344cb94e6b
Alesa95 <alejandra_santiago_95@hotmail.com>
2024-10-28 13:41:18 +0100

    formulario usuario con bootstrap

---
+++ 04_formularios/usuario.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <?php
+        error_reporting( E_ALL );
+        ini_set("display_errors", 1 );  
+    ?>
+    <style>
+        .error {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <!-- Content here -->
+
+        <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+            $tmp_usuario = $_POST["usuario"];
+            $tmp_nombre = $_POST["nombre"];
+            $tmp_apellidos = $_POST["apellidos"];
+
+            if($tmp_usuario == '') {
+                $err_usuario = "El usuario es obligatorio";
+            }
+        }
+        ?>
+
+        <h1>Formulario usuario</h1>
+
+        <form class="col-4" action="" method="post">
+            <div class="mb-3">
+                <label class="form-label">Usuario</label>
+                <input class="form-control" type="text" name="usuario">
+                <?php if(isset($err_usuario)) echo "<span class='error'>$err_usuario</span>" ?>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Nombre</label>
+                <input class="form-control" type="text" name="nombre">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Apellidos</label>
+                <input class="form-control" type="text" name="apellidos">
+            </div>
+            <div class="mb-3">
+                <input class="btn btn-primary" type="submit" value="Enviar">
+            </div>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
