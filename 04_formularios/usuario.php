f58388a395056c55195712783b4a16c68b5d4f4b
Alesa95 <alejandra_santiago_95@hotmail.com>
2024-10-30 09:10:50 +0100

    cositas del dni

--- 04_formularios/usuario.php
+++ 04_formularios/usuario.php
@@ -24,6 +24,54 @@
             $tmp_usuario = $_POST["usuario"];
             $tmp_nombre = $_POST["nombre"];
             $tmp_apellidos = $_POST["apellidos"];
+            $tmp_dni = $_POST["dni"];
+
+            if($tmp_dni == '') {
+                $err_dni = "El DNI es obligatorio";
+            } else {
+                $tmp_dni = strtoupper($tmp_dni);
+                $patron = "/^[0-9]{8}[A-Z]$/";
+                if(!preg_match($patron,$tmp_dni)) {
+                    $err_dni = "El DNI debe tener 8 dígitos y una letra";
+                } else {
+                    $numero_dni = (int)substr($tmp_dni,0,8);
+                    $letra_dni = substr($tmp_dni,8,1);
+
+                    $resto_dni = $numero_dni % 23;
+
+                    $letra_correcta = match($resto_dni) {
+                        0 => "T",
+                        1 => "R",
+                        2 => "W",
+                        3 => "A",
+                        4 => "G",
+                        5 => "M",
+                        6 => "Y",
+                        7 => "F",
+                        8 => "P",
+                        9 => "D",
+                        10 => "X",
+                        11 => "B",
+                        12 => "N",
+                        13 => "J",
+                        14 => "Z",
+                        15 => "S",
+                        16 => "Q",
+                        17 => "V",
+                        18 => "H",
+                        19 => "L",
+                        20 => "C",
+                        21 => "K",
+                        22 => "E"
+                    };
+
+                    if($letra_dni != $letra_correcta) {
+                        $err_dni = "La letra del DNI no es correcta";
+                    } else {
+                        $dni = $tmp_dni;
+                    }
+                }
+            }
 
             if($tmp_usuario == '') {
                 $err_usuario = "El usuario es obligatorio";
@@ -60,6 +108,11 @@
         <h1>Formulario usuario</h1>
 
         <form class="col-4" action="" method="post">
+            <div class="mb-3">
+                <label class="form-label">DNI</label>
+                <input class="form-control" type="text" name="dni">
+                <?php if(isset($err_dni)) echo "<span class='error'>$err_dni</span>" ?>
+            </div>
             <div class="mb-3">
                 <label class="form-label">Usuario</label>
                 <input class="form-control" type="text" name="usuario">
