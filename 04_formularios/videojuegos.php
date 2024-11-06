c3f3334293cde5b89fb79ad8a76fa7f3c2282988
Alesa95 <alejandra_santiago_95@hotmail.com>
2024-11-06 09:43:22 +0100

    cositas

--- 04_formularios/videojuegos.php
+++ 04_formularios/videojuegos.php
@@ -23,13 +23,29 @@
         } else {
             $tmp_consola = "";
         }
+        $tmp_fecha_lanzamiento = $_POST["fecha_lanzamiento"];
 
         if($tmp_consola == '') {
             $err_consola = "La consola es obligatoria";
         } else {
-
+            $consolas_validas = ["ps4","ps5","switch","xboxsx"];
+            if(!in_array($tmp_consola,$consolas_validas)) {
+                $err_consola = "La consola no es válida";
+            } else {
+                $consola = $tmp_consola;
+            }
         }
 
+        if($tmp_fecha_lanzamiento == '') {
+            $err_fecha_lanzamiento = "La fecha de lanzamiento es obligatoria";
+        } else {
+            $patron = "/^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/";
+            if(!preg_match($patron, $tmp_fecha_lanzamiento)) {
+                $err_fecha_lanzamiento = "Formato de fecha incorrecto";
+            } else {
+                
+            }
+        }
     }
     ?>
     <div class="container">
@@ -53,6 +69,11 @@
                     <label class="form-check-label">XBox Series X/S</label>
                 </div>
                 <?php if(isset($err_consola)) echo "<span class='error'>$err_consola</span>" ?>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Fecha de lanzamiento</label>
+                <input class="form-control" type="date" name="fecha_lanzamiento">
+                <?php if(isset($err_fecha_lanzamiento)) echo "<span class='error'>$err_fecha_lanzamiento</span>" ?>
             </div>
             <div class="mb-3">
                 <input class="btn btn-primary" type="submit" value="Enviar">
