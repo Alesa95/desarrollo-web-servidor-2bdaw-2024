ef3d37e6bc6ef59383348d6982335cc661291902
Alesa95 <alejandra_santiago_95@hotmail.com>
2024-11-11 13:19:49 +0100

    cositas

---
+++ 06_bases_de_datos/animes/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index de Animes</title>
+    <?php
+        error_reporting( E_ALL );
+        ini_set("display_errors", 1 );    
+
+        require('conexion.php');
+    ?>
+</head>
+<body>
+    <h1>Tabla de animes</h1>
+    <?php
+        
+    ?>
+</body>
+</html>
