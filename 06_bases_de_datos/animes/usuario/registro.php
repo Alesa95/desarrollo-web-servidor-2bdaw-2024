9ffc8cb2598fcffd5f428923f94a38bf76ce9695
Alesa95 <alejandra_santiago_95@hotmail.com>
2024-11-25 12:39:26 +0100

    formulario de registro

---
+++ 06_bases_de_datos/animes/usuario/registro.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro   </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <?php
+        error_reporting( E_ALL );
+        ini_set("display_errors", 1 );    
+
+        require('../conexion.php');
+    ?>
+</head>
+<body>
+    <div class="container">
+        <h1>Registro</h1>
+        
+        <form class="col-6" action="" method="post" enctype="multipart/form-data">
+            <div class="mb-3">
+                <label class="form-label">Usuario</label>
+                <input class="form-control" type="text" name="usuario">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Contraseña</label>
+                <input class="form-control" type="password" name="contrasena">
+            </div>
+            <div class="mb-3">
+                <input class="btn btn-primary" type="submit" value="Registrarse">
+                <a class="btn btn-secondary" href="../index.php">Volver</a>
+            </div>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
