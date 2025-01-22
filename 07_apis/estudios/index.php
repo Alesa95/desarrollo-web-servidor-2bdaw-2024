da1470afac46d87ceea43476f19cbbadaf0a67b2
Alesa95 <alejandra_santiago_95@hotmail.com>
2025-01-22 09:14:43 +0100

    lectura del endpoint

---
+++ 07_apis/estudios/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudios</title>
+    <?php
+        error_reporting( E_ALL );
+        ini_set("display_errors", 1 );    
+    ?>
+</head>
+<body>
+    <?php
+    $apiUrl = "http://localhost/Ejercicios/07_apis/estudios/api_estudios.php";
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $apiUrl);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $respuesta = curl_exec($curl);
+    curl_close($curl);
+
+    $estudios = json_decode($respuesta, true);
+    print_r($estudios);
+    ?>
+</body>
+</html>
