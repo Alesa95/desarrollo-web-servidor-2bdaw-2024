3c3781ac9129419b8ab25d27e9e522154cc50d82
Alesa95 <alejandra_santiago_95@hotmail.com>
2025-01-27 12:47:03 +0100

    anime.php empezado

--- 07_apis/jikan/anime.php
+++ 07_apis/jikan/anime.php
@@ -3,9 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Anime</title>
+    <?php
+        error_reporting( E_ALL );
+        ini_set("display_errors", 1 );    
+    ?>
 </head>
 <body>
-    
+    <?php
+        $id = $_GET["id"];
+        $apiUrl = "https://api.jikan.moe/v4/anime/$id";
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $apiUrl);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $respuesta = curl_exec($curl);
+        curl_close($curl);
+
+        $datos = json_decode($respuesta, true);
+        $anime = $datos["data"];
+    ?>
+    <h1><?php echo $anime["title"] ?></h1>
 </body>
 </html>
--- 07_apis/jikan/top_anime.php
+++ 07_apis/jikan/top_anime.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Top Anime</title>
+    <?php
+        error_reporting( E_ALL );
+        ini_set("display_errors", 1 );    
+    ?>
 </head>
 <body>
     <?php
